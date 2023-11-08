0b81e398c9e63a16f749b51988da384c404dd678
MedHachami <moha.hach.9@gmail.com>
2023-11-09 00:49:18 +0100

    upd config

--- config.php
+++ config.php
@@ -1,6 +1,14 @@
 <?php
-$serveur = "localhost";
-$utilisateur_db = "root";
-$mot_de_passe_db = "";
-$base_de_donnees = "todo list";
-?>
+
+
+define('DB_SERVER','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME','todo list');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($link == false){
+    mysqli_connect_error();
+    die("Error: could not connect");
+}
