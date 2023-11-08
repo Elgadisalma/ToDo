63e731d1f2d08c16aceaed55d00a4bc729ec7f11
Elgadisalma <elgadi.salma0@gmail.com>
2023-11-09 00:53:41 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,4 +3,6 @@ $serveur = "localhost";
 $utilisateur_db = "root";
 $mot_de_passe_db = "";
 $base_de_donnees = "todo list";
+
+$connexion = mysqli_connect($serveur, $utilisateur_db, $mot_de_passe_db, $base_de_donnees);
 ?>
