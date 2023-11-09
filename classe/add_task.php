385c2f22452c26eb3e3a95a73a67ef9083b8b80c
MedHachami <moha.hach.9@gmail.com>
2023-11-09 17:13:55 +0100

    Update add_task.php

--- classe/add_task.php
+++ classe/add_task.php
@@ -1,23 +1,23 @@
 <?php
 require_once 'config.php';
 
-if ($link->connect_error) {
-    die("La connexion a échoué : " . $link->connect_error);
+if(isset($_POST['submit'])){
+    $taskName = $_POST['taskName'];
+    $taskDescription = $_POST['taskDescription'];
+    $taskDeadline = $_POST['taskDeadline'];
+
+    $sql = "INSERT INTO tasks (nom_tache, description_tache, deadline) VALUES ('$taskName', '$taskDescription', '$taskDeadline')";
+
+    if ($link->query($sql) === TRUE) {
+        // echo "La tâche a été ajoutée avec succès!";
+        header("location:../index.php?task=added_successfully");
+        exit();
+    } else {
+        echo "Erreur: " . $link->error;
+    }
+
+    $link->close();
 }
 
-$taskName = $_POST['taskName'];
-$taskDescription = $_POST['taskDescription'];
-$taskDeadline = $_POST['taskDeadline'];
 
-$sql = "INSERT INTO tasks (nom_tache, description_tache, deadline) VALUES ('$taskName', '$taskDescription', '$taskDeadline')";
-
-if ($link->query($sql) === TRUE) {
-    // echo "La tâche a été ajoutée avec succès!";
-    header("location:../index.html?task=added_successfully");
-    exit();
-} else {
-    echo "Erreur: " . $link->error;
-}
-
-$link->close();
 ?>
